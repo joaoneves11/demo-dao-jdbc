6a6a3e458fecd229437ab6d3cf7f8df919852e0c
João Neves <joaopedro@outlook.com.br>
2024-02-01 11:08:40 -0300

    seller entity class

--- src/application/program.java
+++ src/application/program.java
@@ -1,56 +1,19 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
 
-		Connection conn = null;
-		Statement st = null;
-		try {
-			conn = DB.getConnection();
-			
-			conn.setAutoCommit(false);//todas operações por padrão vão ficar pendente de uma confirmação
-			
-			
-			st = conn.createStatement();
 
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2050 WHERE DepartmentId = 1");
-			int x = 1;
-
-			if (x < 2) {
-				throw new SQLException("Fake Error");
-			}
-
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
-
-			conn.commit();
-			
-			System.out.println("rows1: " + rows1);
-			System.out.println("rows2: " + rows2);
-
-		} catch (SQLException e) {
-			try {
-				conn.rollback();
-				throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
-				
-			} catch (SQLException e1) {
-				throw new DbException("Error trying rollback! Caused by: " + e1.getMessage());
-			}
-		} finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		
+		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Boob", "boob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
+		
 	}
 }
