5cdf5ceefe9bfbee609957342d5d2556df415ae4
João Neves <joaopedro@outlook.com.br>
2024-02-01 12:21:25 -0300

    reusing instatiation

--- src/application/program.java
+++ src/application/program.java
@@ -7,10 +7,9 @@ import model.entities.Seller;
 public class program {
 
 	public static void main(String[] args) {
-
-
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("=== Test 1: seller findyById ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
